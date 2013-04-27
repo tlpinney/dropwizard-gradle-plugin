f3c4437fd8485cf4bb4a0e174ee751063390ee4e
Travis Camechis <tcamechis@bericotechnologies.com>
2013-04-26 20:07:32 -0400

    hooked into the jar standard java jar task to add all deps into a lib dir in the jar

--- src/main/groovy/com/berico/gradle/dropwizard/plugin/DropWizardPlugin.groovy
+++ src/main/groovy/com/berico/gradle/dropwizard/plugin/DropWizardPlugin.groovy
@@ -1,24 +1,29 @@
 package com.berico.gradle.dropwizard.plugin
 
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.internal.file.copy.CopyAction;
+import org.gradle.api.internal.file.copy.CopySpecImpl;
 import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.TaskContainer;
+import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+
 class DropWizardPlugin implements Plugin<Project> {
 
-	
+
 	private Project project
-	
+
 	private DropWizardPluginExtension pluginExtension
-	
+
 	public void apply(Project project) {
-	
+
 		this.project = project
 		project.apply(plugin: 'java')
 		addPluginConvention()
 		addJavaExecTask()
-		
-		
+		addFatJarTask()
 	}
 
 	private void addPluginConvention() {
@@ -30,9 +35,18 @@ class DropWizardPlugin implements Plugin<Project> {
 	private void addJavaExecTask( ) {
 		def run = project.tasks.add("run", JavaExec)
 		run.description = "Runs this project as a JVM application"
-		run.group = "application"
+		run.group = "dropwizard"
 		run.classpath = project.sourceSets.main.runtimeClasspath
 		run.conventionMapping.main = { pluginExtension.mainClassName }
 	}
-	
+
+	private void addFatJarTask() {
+		def tasks = project.getTasksByName("jar", false)
+		for( i in tasks ) {
+			Jar jar = (Jar)i
+			CopySpecImpl lib = jar.getCopyAction().rootSpec.addFirst().into('lib')
+
+			lib.addChild().from(project.configurations.runtime)
+		}
+	}
 }
