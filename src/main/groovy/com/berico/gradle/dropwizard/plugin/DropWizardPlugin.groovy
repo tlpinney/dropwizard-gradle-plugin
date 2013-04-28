addc1804eacf6d8dd6e73e40b22da69d2d6a0404
Travis Camechis <tcamechis@bericotechnologies.com>
2013-04-27 20:56:55 -0400

    added capability to create a tar distribution

--- src/main/groovy/com/berico/gradle/dropwizard/plugin/DropWizardPlugin.groovy
+++ src/main/groovy/com/berico/gradle/dropwizard/plugin/DropWizardPlugin.groovy
@@ -2,8 +2,9 @@ package com.berico.gradle.dropwizard.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.CopySpec
 import org.gradle.api.tasks.JavaExec
-
+import org.gradle.api.tasks.bundling.Tar
 import eu.appsatori.gradle.fatjar.FatJarPlugin
 
 
@@ -13,6 +14,10 @@ class DropWizardPlugin implements Plugin<Project> {
 	private Project project
 
 	private DropWizardPluginExtension pluginExtension
+	
+	static final String TASK_DIST_TAR_NAME = "distTar"
+	
+	static final String DROPWIZ_GROUP = "dropwizard"
 
 	public void apply(Project project) {
 		project.extensions.create('dropwizard', DropWizardPluginExtension)
@@ -21,6 +26,7 @@ class DropWizardPlugin implements Plugin<Project> {
 		project.plugins.apply FatJarPlugin
 		addJavaExecTask()
 		addFatJarTask()
+		addArchiveTask()
 	}
 
 
@@ -35,6 +41,37 @@ class DropWizardPlugin implements Plugin<Project> {
 	def addFatJarTask( ) {
 		def fatjar = project.tasks.findByName("fatJar")
 		fatjar.group = "dropwizard"
+	}
+	
+	def addArchiveTask( ) {
+		def archiveTask = project.tasks.add(TASK_DIST_TAR_NAME, Tar )
+		archiveTask.description = "Bundles the project as a JVM application with libs and OS specific scripts."
+        archiveTask.group = DROPWIZ_GROUP
+        archiveTask.conventionMapping.baseName = { project.name }
+        def baseDir = { archiveTask.archiveName - ".${archiveTask.extension}" }
+        archiveTask.into(baseDir) {
+            with(configureDistSpec())
+        }
+	}
+	
+	private CopySpec configureDistSpec() {
+		def distSpec = project.copySpec {}
+		def jar = project.tasks["fatJar"]
+		//def startScripts = project.tasks[TASK_START_SCRIPTS_NAME]
+
+		distSpec.with {
+			from(project.file("src/dist"))
+
+			into("lib") {
+				from(jar)
+			}
+			//into("bin") {
+			//	from(startScripts)
+			//	fileMode = 0755
+			//}
+		}
+
+		distSpec
 	}
 
 
